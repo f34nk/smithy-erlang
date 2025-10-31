e37c71c5b3b5499c7708c77117b99c9000915cd6
Frank Eickhoff <info@frankeickhoff.de>
2025-10-31 19:12:56 +0100

    feat: add smithy-smoke-test-traits dependency
    Add support for @smokeTests traits used in AWS models for basic
    connectivity tests.

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
     implementation("software.amazon.smithy:smithy-protocol-traits:1.51.0")
     implementation("software.amazon.smithy:smithy-rules-engine:1.51.0")
     implementation("software.amazon.smithy:smithy-waiters:1.51.0")
+    implementation("software.amazon.smithy:smithy-smoke-test-traits:1.51.0")
     
     // Testing
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
